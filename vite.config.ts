81b710fcb57fe60a05c6060aeb6bff794684df3d
harsh-2O <202051117@iiitvadodara.ac.in>
2025-04-25 18:33:58 +0530

    Portfolio Image name change2

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
+\
+// vite.config.ts
 export default defineConfig({
-  plugins: [react()],
-})
+  assetsInclude: ['**/*.jpg', '**/*.png', '**/*.jpeg']
+});
